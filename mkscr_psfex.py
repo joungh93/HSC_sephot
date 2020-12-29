8bcab4edb9d83fb660c57c6f10ce87c5ce73598a
Jeong Hwan Lee <40652753+joungh93@users.noreply.github.com>
2020-12-29 17:30:58 +0900

    Update mkscr_psfex.py

--- mkscr_psfex.py
+++ mkscr_psfex.py
@@ -11,13 +11,15 @@ import time
 start_time = time.time()
 
 import numpy as np
-import os
+import glob, os
 from astropy.io import fits
 
 import init_cfg as ic
 
 
 # ----- Making scripts for PSFEx ----- #
+os.system("psfex -dd > config.psfex")
+
 if ic.use_backsub:
 	prefix = 'b'
 else:
@@ -30,10 +32,11 @@ f.write('##### Scripts for PSFEx #####'+'\n')
 f.write('#############################'+'\n')
 f.write('\n')
 for i in np.arange(len(ic.fields)):
-	f.write('# ----- HSC field : '+ic.fields[i]+'----- #'+'\n')
+	f.write('# ----- HSC field : '+ic.fields[i]+' ----- #'+'\n')
 	f.write('\n')
 	for j in np.arange(len(ic.filters)):
 		flt = ic.filters[j].split('-')[1]
+		f.write('rm -rfv prepsfex_'+flt+'.cat\n')
 		f.write('sex Images/'+prefix+ic.fields[i]+'-'+flt+'.fits -c prepsfex.sex -CATALOG_NAME prepsfex_'+flt+'.cat ')
 		f.write('-DETECT_THRESH {0:.1f} -ANALYSIS_THRESH {0:.1f} '.format(ic.THRES_psf))
 		f.write(f"-MAG_ZEROPOINT {ic.MAG0:.1f} -GAIN {ic.GAIN0[j]:.1f} -SEEING_FWHM {ic.SEEING0:.2f}\n")
@@ -43,15 +46,25 @@ for i in np.arange(len(ic.fields)):
 		f.write('psfex prepsfex_'+flt+'.cat -c config.psfex ')
 		f.write(f"-SAMPLE_FWHMRANGE {ic.FWHMR_psf[0]:.1f},{ic.FWHMR_psf[1]:.1f} ")
 		f.write(f"-SAMPLE_MINSN {ic.MINSN_psf:.1f} -SAMPLE_MAXELLIP {ic.MAXEL_psf:.2f} ")
-		f.write('-OUTCAT_NAME psf_'+ic.fields[i]+'-'+flt+'.cat\n')
+		f.write('-OUTCAT_TYPE ASCII_HEAD -OUTCAT_NAME psf_'+ic.fields[i]+'-'+flt+'.cat ')
+		f.write('-CHECKPLOT_TYPE NONE -XML_NAME psf_'+ic.fields[i]+'-'+flt+'.xml\n')
+		f.write('mv -v prepsfex_'+flt+'.psf psf_'+ic.fields[i]+'-'+flt+'.psf')
 		f.write('\n')
 	f.write('\n\n')
-
 f.close()
 
 
 # ----- Running scripts for PSFEx ----- #
-os.system('sh psfex_all.sh')    
+if (glob.glob("PSFEx/") == []):
+	os.system("mkdir PSFEx")
+else:
+	os.system("rm -rfv PSFEx/*")
+
+os.system("sh psfex_all.sh")
+
+os.system("mv -v psf_*.cat psf_*.xml psf_*.psf PSFEx/")
+os.system("mv -v prepsfex_*-*.cat PSFEx/")
+os.system("rm -rfv ./*.fits prepsfex_*.cat")
 
 
 # Printing the running time  
