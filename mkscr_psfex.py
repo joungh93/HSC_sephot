d2dd208f0645340ab2e2200795e19f3ee0761758
Jeong Hwan Lee <40652753+joungh93@users.noreply.github.com>
2020-12-29 09:33:47 +0900

    Update mkscr_psfex.py

--- mkscr_psfex.py
+++ mkscr_psfex.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 """
-Created on Wed Aug 28 09:46:55 2019
+Created on Thu May 21 11:40:26 2020
 
 @author: jlee
 """
@@ -14,31 +14,39 @@ import numpy as np
 import os
 from astropy.io import fits
 
-import init_patch as ip
+import init_cfg as ic
 
 
 # ----- Making scripts for PSFEx ----- #
+if ic.use_backsub:
+	prefix = 'b'
+else:
+	prefix = ''
+
 f = open('psfex_all.sh','w')
 f.write('\n')
 f.write('#############################'+'\n')
 f.write('##### Scripts for PSFEx #####'+'\n')
 f.write('#############################'+'\n')
 f.write('\n')
-for i in np.arange(len(ip.patchstr_dir)):
-	f.write('# ----- HSC patch : '+ip.patchstr_img[i]+'----- #'+'\n')
+for i in np.arange(len(ic.fields)):
+	f.write('# ----- HSC field : '+ic.fields[i]+'----- #'+'\n')
 	f.write('\n')
-	f.write('sex G'+ip.patchstr_sex[i]+'.fits -c prepsfex.sex -CATALOG_NAME prepsfex_g.cat -DETECT_THRESH 10.0 -ANALYSIS_THRESH 10.0 -MAG_ZEROPOINT 27.0 -GAIN 12780.0 -SEEING_FWHM 4.10'+'\n')
-	f.write('sex G'+ip.patchstr_sex[i]+'.fits -c prepsfex.sex -CATALOG_NAME prepsfex_G'+ip.patchstr_sex[i]+'.cat -CATALOG_TYPE ASCII_HEAD -DETECT_THRESH 10.0 -ANALYSIS_THRESH 10.0 -MAG_ZEROPOINT 27.0 -GAIN 12780.0 -SEEING_FWHM 4.10'+'\n')
-	f.write('psfex prepsfex_g.cat -c config.psfex -SAMPLE_FWHMRANGE 3.0,5.0 -SAMPLE_MINSN 5.0 -SAMPLE_MAXELLIP 0.18 -OUTCAT_NAME psf_G'+ip.patchstr_sex[i]+'.cat'+'\n')
-	f.write('\n')
-	f.write('sex R'+ip.patchstr_sex[i]+'.fits -c prepsfex.sex -CATALOG_NAME prepsfex_r.cat -DETECT_THRESH 10.0 -ANALYSIS_THRESH 10.0 -MAG_ZEROPOINT 27.0 -GAIN 10080.0 -SEEING_FWHM 4.00'+'\n')
-	f.write('sex R'+ip.patchstr_sex[i]+'.fits -c prepsfex.sex -CATALOG_NAME prepsfex_R'+ip.patchstr_sex[i]+'.cat -CATALOG_TYPE ASCII_HEAD -DETECT_THRESH 10.0 -ANALYSIS_THRESH 10.0 -MAG_ZEROPOINT 27.0 -GAIN 10080.0 -SEEING_FWHM 4.00'+'\n')
-	f.write('psfex prepsfex_r.cat -c config.psfex -SAMPLE_FWHMRANGE 3.0,5.0 -SAMPLE_MINSN 5.0 -SAMPLE_MAXELLIP 0.18 -OUTCAT_NAME psf_R'+ip.patchstr_sex[i]+'.cat'+'\n')
-	f.write('\n')
-	f.write('sex I'+ip.patchstr_sex[i]+'.fits -c prepsfex.sex -CATALOG_NAME prepsfex_i.cat -DETECT_THRESH 10.0 -ANALYSIS_THRESH 10.0 -MAG_ZEROPOINT 27.0 -GAIN 10080.0 -SEEING_FWHM 4.40'+'\n')
-	f.write('sex I'+ip.patchstr_sex[i]+'.fits -c prepsfex.sex -CATALOG_NAME prepsfex_I'+ip.patchstr_sex[i]+'.cat -CATALOG_TYPE ASCII_HEAD -DETECT_THRESH 10.0 -ANALYSIS_THRESH 10.0 -MAG_ZEROPOINT 27.0 -GAIN 10080.0 -SEEING_FWHM 4.40'+'\n')
-	f.write('psfex prepsfex_i.cat -c config.psfex -SAMPLE_FWHMRANGE 3.0,5.0 -SAMPLE_MINSN 5.0 -SAMPLE_MAXELLIP 0.18 -OUTCAT_NAME psf_I'+ip.patchstr_sex[i]+'.cat'+'\n')
+	for j in np.arange(len(ic.filters)):
+		flt = ic.filters[j].split('-')[1]
+		f.write('sex Images/'+prefix+ic.fields[i]+'-'+flt+'.fits -c prepsfex.sex -CATALOG_NAME prepsfex_'+flt+'.cat ')
+		f.write('-DETECT_THRESH {0:.1f} -ANALYSIS_THRESH {0:.1f} '.format(ic.THRES_psf))
+		f.write(f"-MAG_ZEROPOINT {ic.MAG0:.1f} -GAIN {ic.GAIN0[j]:.1f} -SEEING_FWHM {ic.SEEING0:.2f}\n")
+		f.write('sex Images/'+prefix+ic.fields[i]+'-'+flt+'.fits -c prepsfex.sex -CATALOG_NAME prepsfex_'+ic.fields[i]+'-'+flt+'.cat -CATALOG_TYPE ASCII_HEAD ')
+		f.write('-DETECT_THRESH {0:.1f} -ANALYSIS_THRESH {0:.1f} '.format(ic.THRES_psf))
+		f.write(f"-MAG_ZEROPOINT {ic.MAG0:.1f} -GAIN {ic.GAIN0[j]:.1f} -SEEING_FWHM {ic.SEEING0:.2f}\n")
+		f.write('psfex prepsfex_'+flt+'.cat -c config.psfex ')
+		f.write(f"-SAMPLE_FWHMRANGE {ic.FWHMR_psf[0]:.1f},{ic.FWHMR_psf[1]:.1f} ")
+		f.write(f"-SAMPLE_MINSN {ic.MINSN_psf:.1f} -SAMPLE_MAXELLIP {ic.MAXEL_psf:.2f} ")
+		f.write('-OUTCAT_NAME psf_'+ic.fields[i]+'-'+flt+'.cat\n')
+		f.write('\n')
 	f.write('\n\n')
+
 f.close()
 
 
