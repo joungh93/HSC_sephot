3e777b00545353ce43116996dd879c51957d1655
Jeong Hwan Lee <40652753+joungh93@users.noreply.github.com>
2020-12-30 15:51:49 +0900

    Update init_cfg.py

--- init_cfg.py
+++ init_cfg.py
@@ -21,21 +21,22 @@ dec0 = ["+69:44:49.5", "+68:46:21.6"]
 # Command: ds9 -mosaic wcs [dir_coadd]/HSC-[filter]/[tract]/*,*/calexp*.fits &
 pixscale = 0.168   # arcsec/pix
 isize = 1.6*3600.0 / pixscale    # Image size (total: 1.6 arcdeg)
-upper_limit = 10.    # upper limit of pixel values
+upper_limit = 10.0    # upper limit of pixel values
+BACKS_swp = 64    # BACK_SIZE for SWarp configuration
 
 
 # ----- Photometric parameters ----- #
 
 ## General
-use_backsub = False  # if True, SExtractor photometry will be applied to background-subtracted images
+use_backsub = True  # if True, SExtractor photometry will be applied to background-subtracted images
 MAG0 = 27.0  # MAG_ZEROPOINT
 SEEING0 = 0.7  # SEEING_FWHM
 GAIN0 = [12600, 9900, 9900]  # effective gain
 
 ## PSFEx
-THRES_psf = 10  # DETECT_THRESH
+THRES_psf = 10.0  # DETECT_THRESH
 FWHMR_psf = [3.0, 5.0]  # SAMPLE_FWHMRANGE
-MINSN_psf = 5.0  # SAMPLE_MINSN
+MINSN_psf = 10.0  # SAMPLE_MINSN
 MAXEL_psf = 0.18  # SAMPLE_MAXELLIP
 
 ## SExtractor
